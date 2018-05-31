d7fcdd5f02f3eb03fa1d488f944670674025d772
RohitoOo <bhambhani.ba@gmail.com>
2018-05-31 10:49:00 +0530

    Promises

--- index.js
+++ index.js
@@ -1,65 +1,60 @@
-let promiseToCleanRoom = new Promise(function(resolve,reject){
+let promiseToCleanRoom = new Promise(function(resolve, reject) {
 
   let isClean = true;
 
-// let isClean = false;
+  // let isClean = false;
 
-  if(isClean){
+  if (isClean) {
     resolve("Clean")
-  }else{
+  } else {
     reject("Dirtty!")
   }
 
 })
 
-promiseToCleanRoom
-
-.then(function(response){
+promiseToCleanRoom.then(function(response) {
+  console.log("The room is " + response)
+}).catch(function(response) {
   console.log("The room is " + response)
 })
-
-.catch(function(response){
-  console.log("The room is " + response)
-})
-
 
 /// Embedded Promises
 
-
-
-
-let cleanRoom = function(){
-  return new Promise(function(resolve,reject){
-     reject('Cleaned the Room')
-//   resolve('Cleaned the Room')
+let cleanRoom = function() {
+  return new Promise(function(resolve, reject) {
+    reject('Cleaned the Room')
+    //   resolve('Cleaned the Room')
 
   })
 }
 
-let removeGarbage = function(){
-  return new Promise(function(resolve,reject){
+let removeGarbage = function() {
+  return new Promise(function(resolve, reject) {
     reject('Garbage Removed')
-//  resolve('Garbage Removed')
+    //  resolve('Garbage Removed')
 
   })
 }
 
-
-let rewardEarned = function(){
-  return new Promise(function(resolve, reject){
+let rewardEarned = function() {
+  return new Promise(function(resolve, reject) {
     reject("Here's your Cookie")
-//  resolve("Here's your Cookie")
+    //  resolve("Here's your Cookie")
 
   })
 }
 
-cleanRoom().then(function(message){
+cleanRoom().then(function(message) {
   console.log(message)
-      return removeGarbage()
-    }).then(function(message){
-      console.log(message)
-      return rewardEarned()
-    }).then(function(message){
-      console.log(message)
-      console.log("Well Done!")
-    })
+  return removeGarbage()
+}).then(function(message) {
+  console.log(message)
+  return rewardEarned()
+}).then(function(message) {
+  console.log(message)
+  console.log("Well Done!")
+})
+
+Promise.all([cleanRoom(), removeGarbage(), rewardEarned()]).then(function() {
+  console.log("All Promises Done At Once!")
+})
